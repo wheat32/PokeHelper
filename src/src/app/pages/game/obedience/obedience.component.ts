cef468432a77cbbb02a8a4742cddb8ebc6ac8b36
Nick <nickpage3232@yahoo.com>
2026-04-07 20:28:42 -0400

    Update obedience.component.ts

--- src/src/app/pages/game/obedience/obedience.component.ts
+++ src/src/app/pages/game/obedience/obedience.component.ts
@@ -24,12 +24,12 @@ export class ObedienceComponent extends GamePageBase
 {
   obedienceRows: ObedienceRow[] = [
     { badges: 0,  badgeName: 'No Badges',      gymLeader: '—',         city: '—',               levelCap: 10,   isMilestone: true  },
-    { badges: 1,  badgeName: 'Boulder Badge',   gymLeader: 'Brock',     city: 'Pewter City',     levelCap: 10,   isMilestone: false },
+    { badges: 1,  badgeName: 'Boulder Badge',   gymLeader: 'Brock',     city: 'Pewter City',     levelCap: 20,   isMilestone: true  },
     { badges: 2,  badgeName: 'Cascade Badge',   gymLeader: 'Misty',     city: 'Cerulean City',   levelCap: 30,   isMilestone: true  },
-    { badges: 3,  badgeName: 'Thunder Badge',   gymLeader: 'Lt. Surge', city: 'Vermilion City',  levelCap: 30,   isMilestone: false },
+    { badges: 3,  badgeName: 'Thunder Badge',   gymLeader: 'Lt. Surge', city: 'Vermilion City',  levelCap: 40,   isMilestone: true  },
     { badges: 4,  badgeName: 'Rainbow Badge',   gymLeader: 'Erika',     city: 'Celadon City',    levelCap: 50,   isMilestone: true  },
-    { badges: 5,  badgeName: 'Soul Badge',      gymLeader: 'Koga',      city: 'Fuchsia City',    levelCap: 50,   isMilestone: false },
-    { badges: 6,  badgeName: 'Marsh Badge',     gymLeader: 'Sabrina',   city: 'Saffron City',    levelCap: 70,   isMilestone: true  },
+    { badges: 5,  badgeName: 'Soul Badge',      gymLeader: 'Koga',      city: 'Fuchsia City',    levelCap: 70,   isMilestone: true  },
+    { badges: 6,  badgeName: 'Marsh Badge',     gymLeader: 'Sabrina',   city: 'Saffron City',    levelCap: 70,   isMilestone: false },
     { badges: 7,  badgeName: 'Volcano Badge',   gymLeader: 'Blaine',    city: 'Cinnabar Island', levelCap: 70,   isMilestone: false },
     { badges: 8,  badgeName: 'Earth Badge',     gymLeader: 'Giovanni',  city: 'Viridian City',   levelCap: null, isMilestone: true  },
   ];
